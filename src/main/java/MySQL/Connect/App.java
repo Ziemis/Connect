9efc40c5fb30e4d8812a469be0ec650cf998269f
Ziemis <ziemi.siema@gmail.com>
2019-03-23 12:21:28 +0100

    Initial commit

---
+++ src/main/java/MySQL/Connect/App.java
@@ -0,0 +1,13 @@
+package MySQL.Connect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
