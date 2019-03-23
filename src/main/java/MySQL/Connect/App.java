c8b96a854a513c2098d83d23138074a7f6e61ef2
Ziemis <ziemi.siema@gmail.com>
2019-03-23 14:05:53 +0100

    second commit

--- src/main/java/MySQL/Connect/App.java
+++ src/main/java/MySQL/Connect/App.java
@@ -1,13 +1,31 @@
 package MySQL.Connect;
+import java.sql.*;
 
+import com.mysql.cj.jdbc.JdbcPropertySet;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClassNotFoundException
     {
-        System.out.println( "Hello World!" );
+    	//Class.forName("com.mysql.jdbc.Driver");
+    	String url = "jdbc:mysql://localhost:3306/world?useLegacyDatetimeCode=false&serverTimezone=America/New_York";
+    	try(Connection con = DriverManager.getConnection
+    			(url, "root", "haslodomysql321");
+    		Statement stmt = con.createStatement();
+    		ResultSet rs = stmt.executeQuery("SELECT * FROM city")){
+    		
+    		while(rs.next()) {
+    			String s = rs.getString("Name");
+    			System.out.println(s);
+    		}
+    		
+    	}catch(SQLException ex) {
+    		ex.printStackTrace();
+    	}
+    	
+    	
     }
 }
